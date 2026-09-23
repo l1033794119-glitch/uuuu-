431e3504eb6087e8fc21227bf94f3a763edf7258
l1033794119-glitch <l1033794119-glitch@users.noreply.github.com>
2026-09-23 22:36:40 +0800

    feat: add b2.mnxy.me tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -70,7 +71,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      {children}
+      <Script src="https://b2.mnxy.me/u1.js" strategy="afterInteractive" />
+    </body>
     </html>
   );
 }
